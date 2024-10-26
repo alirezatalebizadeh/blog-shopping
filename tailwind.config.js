cd4be0df7c1ad5b48d4716a3f9a6a398c5794f56
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-10-26 12:48:47 +0330

    responsive menu for mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,54 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        normal: "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemiBold: "Dana DemiBold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+      letterSpacing: {
+        tightest: "-.065em",
+      },
+      spacing: {
+        25: "6.25rem",
+        30: "7.5rem",
+        50: "12.5rem",
+        82: "20.5rem",
+      },
+      backgroundImage: {
+        "home-desktop": "url('../images/headerBgDesktop.png')",
+        "home-mobile": "url('../images/headerBgMobile.webp')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "0.625rem",
       },
     },
   },
