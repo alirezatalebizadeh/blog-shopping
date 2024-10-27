bfd479099f4a21d6924ebd55e1f1bdef8890db11
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-10-27 17:19:11 +0330

    debuging

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,7 +12,7 @@ export default function Products({ props }) {
         {/* //!products */}
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5 pb-8 md:pb-20">
           {[1, 2, 3, 4, 5, 6, 7, 8].map((product, index) => (
-            <ProductItem />
+            <ProductItem key={index} />
           ))}
         </div>
       </div>
