6eaa9712335c7b9e736cde5817d516431d559bd8
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-10-27 06:45:43 +0330

    debug styling

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,27 +3,27 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div class="container mt-8 mb-10 md:my-20">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+    <div className="container mt-8 mb-10 md:my-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <Link
           href="#"
-          class="category-banner-item1 flex flex-col justify-center items-start rounded-2xl bg-zinc-700 h-[142px] sm:h-[200px] md:h-[248px] pr-7 md:pr-12"
+          className="category-banner-item1 flex flex-col justify-center items-start rounded-2xl bg-zinc-700 h-[142px] sm:h-[200px] md:h-[248px] pl-7 md:pl-12"
         >
-          <span class="text-white font-DanaDemiBold text-2xl/[24px] md:text-4xl/[24px]">
+          <span className="text-white font-DanaDemiBold text-2xl/[24px] md:text-4xl/[24px]">
             Types of Coffee
           </span>
-          <span class="text-white font-Dana text-base md:text-2xl/[24px] mt-4 md:mt-7">
+          <span className="text-white font-Dana text-base md:text-2xl/[24px] mt-4 md:mt-7">
             Blended and Single-Origin
           </span>
         </Link>
         <Link
           href="#"
-          class="category-banner-item2 flex flex-col justify-center items-start rounded-2xl bg-zinc-700 h-[142px] sm:h-[200px] md:h-[248px] pr-7 md:pr-12"
+          className="category-banner-item2 flex flex-col justify-center items-start rounded-2xl bg-zinc-700 h-[142px] sm:h-[200px] md:h-[248px] pl-7 md:pl-12"
         >
-          <span class="text-white font-DanaDemiBold text-2xl/[24px] md:text-4xl/[24px]">
+          <span className="text-white font-DanaDemiBold text-2xl/[24px] md:text-4xl/[24px]">
             Accessories and Equipment
           </span>
-          <span class="text-white font-Dana text-base md:text-2xl/[24px] mt-4 md:mt-7">
+          <span className="text-white font-Dana text-base md:text-2xl/[24px] mt-4 md:mt-7">
             Nescafe, Hot Chocolate, Masala
           </span>
         </Link>
--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -4,36 +4,36 @@ import React from "react";
 
 export default function CategoryProducts() {
   return (
-    <div class="mb-[118px] mt-20">
-      <div class="container">
-        <div class="flex items-start justify-center flex-wrap gap-y-[36px] gap-x-[21px] md:gap-[65px] text-center">
-          <div class="w-25 md:w-50">
+    <div className="mb-[118px] mt-20">
+      <div className="container">
+        <div className="flex items-start justify-center flex-wrap gap-y-[36px] gap-x-[21px] md:gap-[65px] text-center">
+          <div className="w-25 md:w-50">
             <Link href="#">
               <Image
-                class="w-25 h-25 md:w-50 md:h-50"
+                className="w-25 h-25 md:w-50 md:h-50"
                 loading="lazy"
                 width={200}
                 height={200}
                 src="/images/categories/category1.png"
                 alt="p1"
               />
-              <span class="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
+              <span className="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
                 Brewed Coffee and Espresso
               </span>
             </Link>
           </div>
 
-          <div class="w-25 md:w-50">
+          <div className="w-25 md:w-50">
             <Link href="#">
               <Image
-                class="w-25 h-25 md:w-50 md:h-50"
+                className="w-25 h-25 md:w-50 md:h-50"
                 loading="lazy"
                 width={200}
                 height={200}
                 src="/images/categories/category2.png"
                 alt="p1"
               />
-              <span class="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
+              <span className="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
                 Accessories and Equipment
               </span>
             </Link>
@@ -42,46 +42,46 @@ export default function CategoryProducts() {
           <div>
             <Link href="#">
               <Image
-                class="w-25 h-25 md:w-50 md:h-50"
+                className="w-25 h-25 md:w-50 md:h-50"
                 loading="lazy"
                 width={200}
                 height={200}
                 src="/images/categories/category3.png"
                 alt="p1"
               />
-              <span class="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
+              <span className="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
                 Espresso Machines
               </span>
             </Link>
           </div>
 
-          <div class="w-25 md:w-50">
+          <div className="w-25 md:w-50">
             <Link href="#">
               <Image
-                class="w-25 h-25 md:w-50 md:h-50"
+                className="w-25 h-25 md:w-50 md:h-50"
                 loading="lazy"
                 width={200}
                 height={200}
                 src="/images/categories/category4.png"
                 alt="p1"
               />
-              <span class="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
+              <span className="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
                 Coffee Taster Pack
               </span>
             </Link>
           </div>
 
-          <div class="w-25 md:w-50">
+          <div className="w-25 md:w-50">
             <Link href="#">
               <Image
-                class="w-25 h-25 md:w-50 md:h-50"
+                className="w-25 h-25 md:w-50 md:h-50"
                 loading="lazy"
                 src="/images/categories/category5.png"
                 alt="p1"
                 width={200}
                 height={200}
               />
-              <span class="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
+              <span className="text-zinc-700 md:mt-2.5 dark:text-white font-DanaDemiBold text-sm md:text-xl">
                 Turkish Coffee
               </span>
             </Link>
